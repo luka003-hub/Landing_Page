d27f830616ec36209c02bf1d41750b1a64d4f4aa
brianngumbau <brianngumbau9075@gmail.com>
2024-07-01 22:00:04 +0300

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+
+const app = express();
+const port = 5000;
+
+
+
+app.listen(port, () => {
+    console.log(`landing page is running at http://localhost:${port}`);
+});
